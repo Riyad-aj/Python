af42b93550d0dcbb0168d96736c9f584a481e338
Abdul Jalil Riyad <102417060+ajriyad@users.noreply.github.com>
2022-03-29 01:05:13 +0600

    Create Typecasting.py

---
+++ Typecasting.py
@@ -0,0 +1,30 @@
+x = int(5)
+y = int(3.1416)
+z = int("180")
+print(x)
+print(y)
+print(z)
+x = float(110)
+y = float(2.8)
+z = float("38.7")
+print(x)
+print(y)
+print(z)
+x = str("120")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+___________________________________________&
+5
+3
+180
+110.0
+2.8
+38.7
+120
+2
+3.0
+
+[Program finished]
