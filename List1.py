d8816c1cac784705a30025c100498f72357c6dca
Abdul Jalil Riyad <102417060+ajriyad@users.noreply.github.com>
2022-03-28 23:19:15 +0600

    Update List1.py

--- List1.py
+++ List1.py
@@ -12,3 +12,18 @@ pos=P.count (4)
 print (pos)
 P.clear()
 print (P)
+
+Output_________________________
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+2
+[5, 7, 11]
+29
+False
+False
+True
+True
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+0
+[]
+
+[Program finished]
