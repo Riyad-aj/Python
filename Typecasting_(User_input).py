c47413588bf19a7b5d2ebf311bdba2cc175c24f8
Abdul Jalil Riyad <102417060+Riyad-aj@users.noreply.github.com>
2022-03-29 10:52:42 +0600

    Create Typecasting_(User_input).py

---
+++ Typecasting_(User_input).py
@@ -0,0 +1,19 @@
+P = int(input("Enter the first number:"))
+Q = int(input("Enter the secend number:"))
+print (P*Q)
+print (P%Q)
+print(P // Q)
+print (P==Q)
+print (P>Q)
+print (P<Q)
+______________________________________________
+Enter the first number:50
+Enter the secend number:62
+3100
+50
+0
+False
+False
+True
+
+[Program finished]
