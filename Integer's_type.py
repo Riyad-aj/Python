34128aca8891d8a04968543b9d5018ff5a376d50
Abdul Jalil Riyad <102417060+Riyad-aj@users.noreply.github.com>
2022-03-30 22:23:29 +0600

    Create Integer's_type.py

---
+++ Integer's_type.py
@@ -0,0 +1,12 @@
+a= int(input("Enter a number: "))
+if a>0:
+    print("The number is poitive")
+elif a==0:
+    print("The number is zero")
+else:
+      print("The number is negative")
+__________________________________________
+Enter a number: -20
+The number is negative
+
+[Program finished]
