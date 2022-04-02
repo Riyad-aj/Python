5b005b0b0a118b18fbf9e495da4962afa7195741
Abdul Jalil Riyad <102417060+Riyad-aj@users.noreply.github.com>
2022-04-02 15:30:28 +0600

    Create Trigonometry_2.py

---
+++ Trigonometry_2.py
@@ -0,0 +1,15 @@
+import math
+
+degree = float(input("Enter the value in degrees : "))
+radian = degree * 0.0174533
+
+print("sin({}) : {}".format(degree, math.sin(radian)))
+print("cos({}) : {}".format(degree, math.cos(radian)))
+print("tan({}) : {}".format(degree, math.tan(radian)))
+__________________________________________________
+Enter the value in degrees : 45
+sin(45.0) : 0.7071070192004544
+cos(45.0) : 0.7071065431725605
+tan(45.0) : 1.0000006732053301
+
+[Program finished]
