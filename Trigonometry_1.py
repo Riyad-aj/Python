09287d6b9bcf48810158b6c13d187561f73a6ddb
Abdul Jalil Riyad <102417060+Riyad-aj@users.noreply.github.com>
2022-04-02 14:32:21 +0600

    Create Trigonometry_1.py

---
+++ Trigonometry_1.py
@@ -0,0 +1,18 @@
+import math
+
+print(math.sin(math.pi/3)) 
+print(math.tan(math.pi/4))
+print(math.cos(math.pi/6))
+
+print(math.sin(math.radians(60)))
+print(math.tan(math.radians(45)))
+print(math.cos(math.radians(30)))
+_________________________________________________
+0.8660254037844386
+0.9999999999999999
+0.8660254037844387
+0.8660254037844386
+0.9999999999999999
+0.8660254037844387
+
+[Program finished]
