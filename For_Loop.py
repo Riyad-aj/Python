3b1e2a9e751b63afd52bc68c4aa66213b99aa076
Abdul Jalil Riyad <102417060+Riyadpi@users.noreply.github.com>
2022-04-05 15:53:34 +0600

    Create For_Loop.py

---
+++ For_Loop.py
@@ -0,0 +1,20 @@
+num =[2, 3, 5, 7, 11, 13]
+print (num)
+for x in num:
+    print(x)
+"""
+index = 0
+while index<5 :
+    print (num [index])
+    index= index + 1
+"""
+__________________________________________________
+[2, 3, 5, 7, 11, 13]
+2
+3
+5
+7
+11
+13
+
+[Program finished]
