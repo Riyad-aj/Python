a785b331d3d0997b01eb917fb85e6cdcb9081271
Abdul Jalil Riyad <102417060+Riyadpi@users.noreply.github.com>
2022-04-06 03:57:21 +0600

    Update and rename While_Loop.py to While_Loop_2.py

---
+++ While_Loop_2.py
@@ -0,0 +1,36 @@
+#Print i as long as i is less than 21:
+i = 1
+while i<21:
+    print(i)
+    i=i+5
+#Continue to the next iteration if i is 4:
+i = 0
+while i < 7:
+  i = i+1
+  if i == 4:
+    continue
+  print(i)
+#Exit the loop when i is 7:
+i = 1
+while i < 13:
+  print(i)
+  if i == 7:
+    break
+  i =i+2
+___________________________________________
+1
+6
+11
+16
+1
+2
+3
+5
+6
+7
+1
+3
+5
+7
+
+[Program finished]
