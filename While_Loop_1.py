3f5e80cf04b6412169acf8dd59debbb5364a7158
Abdul Jalil Riyad <102417060+Riyadpi@users.noreply.github.com>
2022-04-06 13:35:52 +0600

    Create While_Loop_1.py

---
+++ While_Loop_1.py
@@ -0,0 +1,19 @@
+count = 0
+while (count < 9):
+   print ("The count number is:", count)
+   count = count + 1
+
+print ("Stop counting")
+_____________________________
+The count number is: 0
+The count number is: 1
+The count number is: 2
+The count number is: 3
+The count number is: 4
+The count number is: 5
+The count number is: 6
+The count number is: 7
+The count number is: 8
+Stop counting
+
+[Program finished]
