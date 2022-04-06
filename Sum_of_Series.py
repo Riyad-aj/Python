39d41329ba195c05a5b8406fcc54242f19fe1bbc
Abdul Jalil Riyad <102417060+Riyadpi@users.noreply.github.com>
2022-04-06 23:14:48 +0600

    Create Sum_of_Series.py

---
+++ Sum_of_Series.py
@@ -0,0 +1,43 @@
+#1+2+3+4+........+n
+num = int(input('Enter a number: '))
+sum = 0
+x = 1
+while x <= num:
+ sum=sum+ x 
+ x=x+1
+print('The sum of natural number =', sum)
+#1²+ 2² + 3² + ... + n²
+num = int(input('Enter a number: '))
+sum = 0
+x = 1
+while x <= num:
+ sum=sum+ x *x
+ x=x+1
+print('The sum of seris=', sum)
+#2² +4² +6²+...+ n²
+num = int(input('Enter a number: '))
+sum = 0
+x = 2
+while x <= num:
+ sum=sum+ x *x
+ x=x+2
+print('The sum of seris=', sum)
+#1+1/2+1/3+........... +1/n
+num = int(input('Enter a number: '))
+sum = 0
+x = 1
+while x <= num:
+ sum=sum+ 1/x
+ x=x+1
+print('The sum of seris=', sum)
+______________________________________________
+Enter a number: 4
+The sum of natural number = 10
+Enter a number: 4
+The sum of seris= 30
+Enter a number: 6
+The sum of seris= 56
+Enter a number: 5
+The sum of seris= 2.283333333333333
+
+[Program finished]
